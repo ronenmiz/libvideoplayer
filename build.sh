7dff061824d3bd78e85363ae8f1c42f893d4d519
Ronen Mizrahi <ronen@tversity.com>
2021-10-21 12:21:11 -0400

    One more cleanup in build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ FFMPEG_LIB_PATH="/usr/local/tversity/x64/lib"
 
 gcc -shared -o libvideoplayer.so cmdutils.c ffplay.c -fPIC -I /usr/include/SDL2 -I /usr/local/include/SDL2 -I $FFMPEG_SRC_PATH -L $FFMPEG_LIB_PATH -lavdevice -lavfilter -lavformat -lavcodec -lavutil -lswscale -lswresample -lSDL2 -lm
 
-gcc main.c -I /usr/local/tversity/x64/include -I /usr/include/SDL2 -I /usr/local/include/SDL2 -L . -lvideoplayer -L /usr/local/tversity/x64/lib -lavdevice -lavfilter -lavformat -lavcodec -lavutil -lswscale -lswresample -lSDL2 -lm
+gcc main.c -I /usr/local/tversity/x64/include -I /usr/include/SDL2 -I /usr/local/include/SDL2 -L . -lvideoplayer -L $FFMPEG_LIB_PATH -lavdevice -lavfilter -lavformat -lavcodec -lavutil -lswscale -lswresample -lSDL2 -lm
